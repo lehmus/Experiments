f1220abc46fd07bd722f0c07282e6918853a1fcf
lehmus <lauri.lehman@gmail.com>
2017-11-11 22:58:16 +0200

    Refactored project names; Reorganized folders

--- DataManager/Datamanager.Automation.Tests/Properties/AssemblyInfo.cs
+++ DataManager/Datamanager.Automation.Tests/Properties/AssemblyInfo.cs
@@ -5,14 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Datamanager.Automation.Tests")]
+[assembly: AssemblyTitle("UnitTestProject1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Datamanager.Automation.Tests")]
+[assembly: AssemblyProduct("UnitTestProject1")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("98f5dd11-99cd-41de-948f-85204a56a3c0")]
 
 // Version information for an assembly consists of the following four values:
 //
